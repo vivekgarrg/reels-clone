1a0774c6e5e5ff96bdec9312fff52152d3d71e02
vivekgarrg <85150074+vivekgarrg@users.noreply.github.com>
2022-04-04 17:42:30 +0530

    seventh commit

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Video.css'
 
 function Video(props) {
+
+    const handleClick = (e) =>{
+          e.preventDefault();
+          e.target.muted = !e.target.muted
+        //   e.target.muted != e.target.muted
+    }
   return (
-     <video src={props.src}  controls>
+     <video src={props.src} muted="muted"  className='videos-styling' onClick={handleClick} controls>
          
      </video>
     
